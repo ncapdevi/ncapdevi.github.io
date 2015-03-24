6a7567fc15d57751fad29cc0f9f9e9603903808d
niccapdevila <ncapdevi@gmail.com>
2015-03-24 12:45:52 -0500

    Testing email

---
+++ sendemail.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['email'])){
+       // from the form
+       $name = trim(strip_tags($_POST['name']));
+       $email = trim(strip_tags($_POST['email']));
+       $message = htmlentities($_POST['message']);
+
+       // set here
+       $subject = "Contact form submitted!";
+       $to = 'Nic.Capdevila@gmail.com';
+
+       $body = <<<HTML
+       $message
+       HTML;
+
+       $headers = "From: $email\r\n";
+       $headers .= "Content-type: text/html\r\n";
+
+       // send the email
+       mail($to, $subject, $body, $headers);
+
+       // redirect afterwords, if needed
+       header('Location: thanks.html');
+}
+?>
